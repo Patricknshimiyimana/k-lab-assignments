60f68541398c47cd0a7159946898270061e32ae4
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-06-25 23:46:29 +0200

    create react app and set environment for calculator app

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="calculator-grid">
-      <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
-      </div>
+ 
     </div>
   );
 }
