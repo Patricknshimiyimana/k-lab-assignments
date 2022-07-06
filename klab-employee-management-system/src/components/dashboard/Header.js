2bab99ce93c143bafc3d740647ad8dac6f9ae4d0
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-07-06 22:46:58 +0200

    export page to pdf and csv files using react-csv and jspdf-autotable

--- klab-employee-management-system/src/components/dashboard/Header.js
+++ klab-employee-management-system/src/components/dashboard/Header.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { employeesData } from "../../data/index";
+import { CSVLink } from "react-csv";
 
 function Header({ setIsAdding }) {
+  const data = [];
+  employeesData.forEach((element) => {
+    data.push([
+      element.id,
+      element.firstName,
+      element.lastName,
+      element.email,
+      element.position,
+    ]);
+  });
 
-    let Navigate = useNavigate();
-    
-    const logoutHandler = () => {
-        localStorage.removeItem("userData");
-        Navigate("/")
-    }
-    return (
-        <header>
-            <div style={{"display": "flex", "justifyContent": "space-between"}}>
-                <h1>Employee Management Software</h1>
-                <Link to={'/'}><button className='round-button' onClick={logoutHandler}>Logout</button></Link>
-            </div>
-            <div style={{ marginTop: '30px', marginBottom: '18px' }}>
-                <button onClick={() => setIsAdding(true)} className='round-button'>Add Employee</button>
-            </div>
-        </header>
-    )
+  const exportToPdfHandler = () => {
+    const doc = new jsPDF();
+    console.log(data);
+    autoTable(doc, {
+      head: [["ID.", "First Name", "Last Name", "Email", "Position"]],
+      body: data, // Must be arrays instead of objects!! that's the reason for Foreach to structure all elements
+    });
+
+    doc.save("klabEmployees.pdf");
+  };
+
+  const fileHeaders = [
+    { label: "First Name", key: "firstName" },
+    { label: "Last Name", key: "lastName" },
+    { label: "Email", key: "email" },
+    { label: "Position", key: "position" },
+  ];
+
+//   const csvData = [[data]];
+
+  const csvReport = {
+    data: employeesData,
+    headers: fileHeaders,
+    filename: 'Klab_employees_list.csv'
+  };
+  console.log(csvReport)
+
+  let Navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userData");
+    Navigate("/");
+  };
+  return (
+    <header>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>Employee Management Software</h1>
+        <Link to={"/"}>
+          <button className="round-button" onClick={logoutHandler}>
+            Logout
+          </button>
+        </Link>
+      </div>
+      <div
+        style={{
+          marginTop: "30px",
+          marginBottom: "18px",
+        }}
+      >
+        <button onClick={() => setIsAdding(true)} className="round-button">
+          Add Employee
+        </button>
+        <button onClick={exportToPdfHandler} className="round-button">
+          Export data to pdf
+        </button>
+        <CSVLink
+          {...csvReport}
+        >
+          <button className="round-button">Export data to csv</button>
+        </CSVLink>
+        ;
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- klab-employee-management-system/src/data/index.js
+++ klab-employee-management-system/src/data/index.js
@@ -4,80 +4,70 @@ const employeesData = [
     firstName: 'Susan',
     lastName: 'Jordon',
     email: 'susan@example.com',
-    position: 'software developer',
-    date: '2019-04-11'
+    position: 'software developer'
   },
   {
     id: 2,
     firstName: 'Adrienne',
     lastName: 'Doak',
     email: 'adrienne@example.com',
-    position: 'software developer',
-    date: '2019-04-17'
+    position: 'software developer'
   },
   {
     id: 3,
     firstName: 'Rolf',
     lastName: 'Hegdal',
     email: 'rolf@example.com',
-    position: 'software developer',
-    date: '2019-05-01'
+    position: 'software developer'
   },
   {
     id: 4,
     firstName: 'Kent',
     lastName: 'Rosner',
     email: 'kent@example.com',
-    position: 'software developer',
-    date: '2019-05-03'
+    position: 'software developer'
   },
   {
     id: 5,
     firstName: 'Arsenio',
     lastName: 'Grant',
     email: 'arsenio@example.com',
-    position: 'software developer',
-    date: '2019-06-13'
+    position: 'software developer'
   },
   {
     id: 6,
     firstName: 'Laurena',
     lastName: 'Lurie',
     email: 'laurena@example.com',
-    position: 'software developer',
-    date: '2019-07-30'
+    position: 'software developer'
   },
   {
     id: 7,
     firstName: 'George',
     lastName: 'Tallman',
     email: 'george@example.com',
-    position: 'software developer',
-    date: '2019-08-15'
+    position: 'software developer'
   },
   {
     id: 8,
     firstName: 'Jesica',
     lastName: 'Watlington',
     email: 'jesica@example.com',
-    position: 'software developer',
-    date: '2019-10-10'
+    position: 'software developer'
   },
   {
     id: 9,
     firstName: 'Matthew',
     lastName: 'Warren',
     email: 'matthew@example.com',
-    position: 'software developer',
-    date: '2019-10-15'
+    position: 'software developer'
   },
   {
     id: 10,
     firstName: 'Lyndsey',
     lastName: 'Follette',
     email: 'lyndsey@example.com',
-    position: 'software developer',
-    date: '2020-01-15'
+    position: 'software developer'
   }
 ];
 
